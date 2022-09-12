270255abb16152598c91d1db61e9cb5c42abc2c1
avnerjose <avnerj.g.r@gmail.com>
2022-09-12 20:43:41 -0300

    Adicionando script para mandar email

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,1 +1,6 @@
 ls
+cd Aula-GitHub-Actions
+echo "Início da instalação"
+sudo apt install mailutils
+echo "Fim da instalação"
+echo "Mandando e-mail com o mail do linux" | mail -s "subject: Achou que eu ia usar meu email pessoal? Muahahahah" chris.souzalima@outlook.com
